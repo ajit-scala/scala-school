73b1530511dee0622f739ae5410ca4340e934a07
Ajit Chahal <ajitchahal@gmail.com>
2016-06-01 23:29:30 +0200

    scala cahce imp

--- scala-cache/app/controllers/HomeController.scala
+++ scala-cache/app/controllers/HomeController.scala
@@ -1,15 +1,20 @@
 package controllers
 
+import java.util.{Calendar, Date}
 import javax.inject._
+
 import play.api._
 import play.api.mvc._
+import play.api.cache._
+
+import scala.concurrent.duration._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject() (cache:CacheApi) extends Controller {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -18,7 +23,17 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    println(cache.get("date"))
+
+    val cd = cache.getOrElse[Date]("date", 10.seconds) {
+      println("getting new date.....")
+      val ld = Calendar.getInstance().getTime
+      cache.set("date", ld, 10.seconds)
+      ld
+    }
+
+
+    Ok(views.html.index("Your new application is ready." + cd))
   }
 
 }
