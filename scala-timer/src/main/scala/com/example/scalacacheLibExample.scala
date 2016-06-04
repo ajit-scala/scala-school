c75a58edd67c5946a9b9dcb91d019cb23ffb7295
Ajit Chahal <ajitchahal@gmail.com>
2016-06-04 03:16:27 +0200

    map to get data from Future[option[obj]]

--- scala-timer/src/main/scala/com/example/scalacacheLibExample.scala
+++ scala-timer/src/main/scala/com/example/scalacacheLibExample.scala
@@ -1,5 +1,7 @@
 package com.example
 
+import java.io.Serializable
+
 import com.sun.net.httpserver.Authenticator.Success
 import net.sf.ehcache.{Cache, CacheManager}
 
@@ -8,6 +10,7 @@ import scalacache.ehcache.EhcacheCache
 import concurrent.duration._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 /**
   * Created by ajit on 03.06.16.
@@ -23,22 +26,43 @@ object scalacacheLibExample {
     val cache:Cache  = cm.getCache("cache1")
     implicit val scalaCache = ScalaCache(EhcacheCache(cache))
 
-    scalaCache.cache.put("t2", Test("Vinit"), Option(15 seconds))
+    scalaCache.cache.put("t2", Test("Vinit"), Option(3 seconds))
     println("::Results::")
 
     var x: Future[Option[Test]] = scalaCache.cache.get[Test]("t2")
 
 
+
+ x.map(opt=>opt.get).onSuccess{//best one ? :) map returns Future of test from futtue of option[test}
+   case res => println(res)
+ }
+
+    Thread.sleep(2 * 1000)
+
+
+    val yy = for{
+      x1<-x if x1.isDefined
+    } yield x1.get
+
+    yy.onSuccess{
+      case y => println("y:"+y)
+    }
+    yy.onFailure{
+      case y => println("yff:"+y)
+    }
+    Thread.sleep(2 * 1000)
+
     x.onSuccess {
       case xx => println(xx.getOrElse("noting "))
     }
-    Thread.sleep(10 * 1000)
+    Thread.sleep(2 * 1000)
      x = scalaCache.cache.get[Test]("t2")
 
     x.onSuccess {
       case xx => println(xx.getOrElse("noting "))
     }
-Thread.sleep(10 * 1000)
+
+    Thread.sleep(3 * 1000)
     x= scalaCache.cache.get[Test]("t2")
 
     x.onSuccess {
