ed271e6467d1f49e28a91312e6f0f2559fda2a36
achahal <ajitchahal@gmail.com>
2016-06-19 11:42:18 +0200

    array blocking queue tests

---
+++ array-blocking-queue/src/main/scala/com/example/Hello.scala
@@ -0,0 +1,10 @@
+package com.example
+
+import java.util.concurrent.ArrayBlockingQueue
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    ArrayBlockingQueue
+    println("Hello, world!")
+  }
+}
